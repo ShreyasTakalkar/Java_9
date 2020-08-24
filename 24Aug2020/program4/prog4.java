4058d55cadaa8aecdc907ce0016c2ab41dd51012
Shreyas Takalkar <shreyas.takalkar@gmail.com>
2020-08-24 21:21:33 +0530

    DailyFlash 24Aug2020

---
+++ 24Aug2020/program4/prog4.java
@@ -0,0 +1,15 @@
+
+class FirstEven {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 100; i++) {
+
+			if(i % 2 == 0) {
+
+				System.out.print(i + " ");
+			}
+		}
+		System.out.println();
+	}
+}
